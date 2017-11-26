52a8e5017638d4bc24c8d794f98783f1a38c0bbf
Andrey Kostakov <bps@dzen.ws>
2017-11-26 15:08:09 +0700

    mooore stability

--- main.go
+++ main.go
@@ -6,10 +6,10 @@ import (
 
 func main() {
 	cmd := runProcess(os.Args[1:])
+	defer gracefulShutdown(cmd)
 	done := make(chan bool)
 	go waitSignals(done)
 	includeDirs := []string{"./"}
 	go runWatch(includeDirs, cmd, os.Args[1:])
 	<-done
-	gracefulShutdown(cmd)
 }
--- watcher.go
+++ watcher.go
@@ -2,11 +2,27 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os/exec"
+	"path/filepath"
 
 	"github.com/fsnotify/fsnotify"
 )
+
+func addRecursively(watcher *fsnotify.Watcher, dir string) {
+	watcher.Add(dir)
+	log.Println("DIR: ", dir)
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		log.Println(err)
+	}
+	for _, file := range files {
+		if file.IsDir() == true {
+			addRecursively(watcher, filepath.Join(dir, file.Name()))
+		}
+	}
+}
 
 func runWatch(includeDirs []string, cmd *exec.Cmd, cmdArgs []string) {
 	watcher, err := fsnotify.NewWatcher()
@@ -16,10 +32,7 @@ func runWatch(includeDirs []string, cmd *exec.Cmd, cmdArgs []string) {
 	defer watcher.Close()
 
 	for _, dir := range includeDirs {
-		err = watcher.Add(dir)
-		if err != nil {
-			log.Fatal(err)
-		}
+		addRecursively(watcher, dir)
 	}
 
 	for {
